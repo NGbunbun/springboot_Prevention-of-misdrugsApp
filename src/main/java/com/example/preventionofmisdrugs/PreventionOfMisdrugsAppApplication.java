efc92f01d469c838a423c55a27ba29c59b4ee34e
NGbunbun <quanbai9@gmail.com>
2024-03-05 06:31:36 +0900

    first commit

---
+++ src/main/java/com/example/preventionofmisdrugs/PreventionOfMisdrugsAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.preventionofmisdrugs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreventionOfMisdrugsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreventionOfMisdrugsAppApplication.class, args);
+	}
+
+}
